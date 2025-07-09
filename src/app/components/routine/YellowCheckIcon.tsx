2c5288ac2fee78485b9f9b8f3103bc784a7e8595
hansanga <cab08132@naver.com>
2025-07-09 17:31:50 +0900

    💄 Style: 루틴페이지 퍼블리싱

---
+++ src/app/components/routine/YellowCheckIcon.tsx
@@ -0,0 +1,23 @@
+
+export default function YellowCheckIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      className="w-[30px] h-[30px] mr-4"
+      fill="#FFB84C"
+      stroke="#FFB84C"
+      strokeWidth="2"
+    >
+      <circle cx="12" cy="12" r="10" fill="#FFB84C" stroke="#FFB84C" />
+      <path
+        d="M9 12l2 2 4-4"
+        fill="none"
+        stroke="#ffffff"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
