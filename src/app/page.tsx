98a43a70e04fe1ecd45b516940a8287b49393cb8
namyoonseo <ysnam0123@naver.com>
2025-07-14 06:15:40 +0900

    알림 모달

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,8 @@ import { useRouter } from 'next/navigation';
 import Quest from './components/main/Quest';
 
 export default function Main() {
-  const [loading, setLoading] = useState(true);
+  // 나중엔 true로 바꿔야함
+  const [loading, setLoading] = useState(false);
   const [openQuest, setOpenQuest] = useState(false);
 
   const router = useRouter();
@@ -21,22 +22,22 @@ export default function Main() {
     router.push('/collection');
   };
 
-  useEffect(() => {
-    try {
-      const fetchData = async () => {
-        // 유저 정보 불러오기 API
-        const userInfo = await me();
-        console.log('유저 정보:', userInfo);
-        // 오늘의 루틴 불러오기 API
-        UserRoutine();
-      };
-      fetchData();
-    } catch (error) {
-      console.error('유저 정보를 불러오지 못했습니다', error);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+  // useEffect(() => {
+  //   try {
+  //     const fetchData = async () => {
+  //       // 유저 정보 불러오기 API
+  //       const userInfo = await me();
+  //       console.log('유저 정보:', userInfo);
+  //       // 오늘의 루틴 불러오기 API
+  //       UserRoutine();
+  //     };
+  //     fetchData();
+  //   } catch (error) {
+  //     console.error('유저 정보를 불러오지 못했습니다', error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }, []);
   return (
     <>
       {loading && <div></div>}
