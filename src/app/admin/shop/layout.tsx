6733f8722ba0d73c0e31bc705afb46dfd1538d34
hansanga <cab08132@naver.com>
2025-07-14 06:43:01 +0900

    📝 Chore: admin 폴더 구조 수정

---
+++ src/app/admin/shop/layout.tsx
@@ -0,0 +1,10 @@
+import BackHeader from '@/app/components/common/ui/BackHeader';
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <BackHeader title="상점 관리" />
+      {children}
+    </>
+  );
+}
