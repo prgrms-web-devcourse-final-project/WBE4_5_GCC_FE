89d864b0c6bf0adf2e335fa106260870e81ea9d5
hansanga <cab08132@naver.com>
2025-07-16 16:06:48 +0900

    ✨ Feat: 프로필부분 api 연결

--- src/api/member.ts
+++ src/api/member.ts
@@ -1,3 +1,4 @@
+import { useUserStore } from '@/store/UserStore';
 import { axiosInstance } from './axiosInstance';
 
 // 유저정보 불러오기
@@ -5,6 +6,11 @@ export const fetchProfile = async () => {
   try {
     const response = await axiosInstance.get('/api/v1/members');
     console.log('회원정보 불러오기 성공', response.data);
+    const { setUser } = useUserStore.getState();
+    setUser({
+      ...response.data.data,
+      isLoggedIn: true,
+    });
     return response.data;
   } catch (error) {
     console.error('회원정보 불러오기 실패', error);
@@ -32,6 +38,53 @@ export const fetchUserQuest = async () => {
     return response.data;
   } catch (error) {
     console.error('보유퀘스트 불러오기 실패', error);
+    throw error;
+  }
+};
+
+// 유저 보유 아이템 불러오기
+export const fetchUserItem = async () => {
+  try {
+    const response = await axiosInstance.get('/api/v1/members/items');
+    console.log('보유아이템 불러오기 성공', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('보유아이템 불러오기 실패', error);
+    throw error;
+  }
+};
+
+// 기존 비밀번호 확인
+export const handleConfirmPassword = async (password: string) => {
+  try {
+    const response = await axiosInstance.post(
+      '/api/v1/members/password',
+      null,
+      {
+        params: { password },
+      },
+    );
+    console.log('비밀번호 확인 성공', response.data);
+    return true;
+  } catch (error) {
+    console.error('비밀번호 확인 실패', error);
+    throw error;
+  }
+};
+
+// 비밀번호 변경
+export const handleChangePassword = async (
+  oldPassword: string,
+  newPassword: string,
+) => {
+  try {
+    const response = await axiosInstance.put('/api/v1/members/password', {
+      oldPassword,
+      newPassword,
+    });
+    console.log('비밀번호 변경 성공', response.data);
+  } catch (error) {
+    console.error('비밀번호 변경 실패', error);
     throw error;
   }
 };
