27187ba5141ae9e06e862ba8dcda383d7be39db1
hansanga <cab08132@naver.com>
2025-07-18 16:55:21 +0900

    🐛 Fix: 닉네임 중복확인 오류 수정

--- src/app/(protected)/mypage/change-userinfo/userinfo/page.tsx
+++ src/app/(protected)/mypage/change-userinfo/userinfo/page.tsx
@@ -83,7 +83,7 @@ export default function Page() {
       return;
     }
     try {
-      await nicknameCheck(nickname);
+      await nicknameCheck(null, nickname);
       setError('');
       setSuccess('사용 가능한 닉네임입니다.');
       setCheckNickName(true);
