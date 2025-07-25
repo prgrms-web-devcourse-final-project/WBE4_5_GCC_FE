b2a72b0db9c1ebf013718f8af5adff2045ffb6d9
hansanga <cab08132@naver.com>
2025-07-26 02:45:10 +0900

    ✨ Feat: 관리자 탈퇴사유분석 페이지 퍼블리싱 완료

---
+++ src/app/admin/withdraw/layout.tsx
@@ -0,0 +1,11 @@
+import BackHeader from '@/app/components/common/ui/BackHeader';
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <BackHeader title="탈퇴 사유 분석" />
+      <hr className="h-[0.5px] border-0 bg-[#CCCCCC]" />
+      {children}
+    </>
+  );
+}
