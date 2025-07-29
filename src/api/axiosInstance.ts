31bcc5c7613bd8158a1462ac7c9fa1b0047de414
seoyeoxxlee <seoyeonglee@umail.ucsb.edu>
2025-07-30 00:22:33 +0900

    병합

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,13 +2,8 @@ import axios from 'axios';
 
 // http://34.61.40.13
 export const axiosInstance = axios.create({
-<<<<<<< HEAD
   baseURL: 'https://honlife.kro.kr',
   //baseURL: 'https://honlife.cedartodo.uk',
-=======
-  // baseURL: 'https://honlife.cedartodo.uk',
-  baseURL: 'https://honlife.kro.kr',
->>>>>>> dev
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
