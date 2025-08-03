3376e5fe5a7b2848f7022e7d1cffffd062cbc14b
namyoonseo <ysnam0123@naver.com>
2025-08-03 19:44:31 +0900

    pwa 잠시 삭제

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,24 @@
-import withPWA from 'next-pwa';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-} as unknown as NextConfig;
+};
 
-const pwaConfig = withPWA({
-  dest: '.next', // 기본값으로 두는 게 좋습니다
-  disable: process.env.NODE_ENV === 'development',
-});
+export default nextConfig;
 
-export default pwaConfig(nextConfig);
+// import withPWA from 'next-pwa';
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//   experimental: {
+//     appDir: true,
+//   },
+// } as unknown as NextConfig;
+
+// const pwaConfig = withPWA({
+//   dest: '.next', // 기본값으로 두는 게 좋습니다
+//   disable: process.env.NODE_ENV === 'development',
+// });
+
+// export default pwaConfig(nextConfig);
