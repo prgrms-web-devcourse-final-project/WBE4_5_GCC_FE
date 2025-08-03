4c835a59757b51109308837b426b91ff977b01c7
namyoonseo <ysnam0123@naver.com>
2025-08-03 19:48:00 +0900

    pwa 복구

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,16 @@
+import withPWA from 'next-pwa';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-};
+  experimental: {
+    appDir: true,
+  },
+} as unknown as NextConfig;
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: '.next', // 기본값으로 두는 게 좋습니다
+  disable: process.env.NODE_ENV === 'development',
+});
 
-// import withPWA from 'next-pwa';
-// import type { NextConfig } from 'next';
-
-// const nextConfig: NextConfig = {
-//   reactStrictMode: true,
-//   experimental: {
-//     appDir: true,
-//   },
-// } as unknown as NextConfig;
-
-// const pwaConfig = withPWA({
-//   dest: '.next', // 기본값으로 두는 게 좋습니다
-//   disable: process.env.NODE_ENV === 'development',
-// });
-
-// export default pwaConfig(nextConfig);
+export default pwaConfig(nextConfig);
